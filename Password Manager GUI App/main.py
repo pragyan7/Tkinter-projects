d98f8eacc884ae8c1e865768cc30d2194d284fd3
Pragyan Dash <68701602+pragyan7@users.noreply.github.com>
2024-06-26 21:34:14 +0530

    Update main.py

--- Password Manager GUI App/main.py
+++ Password Manager GUI App/main.py
@@ -2,11 +2,14 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
-FONT = ("Arial", 10)
-BACKGROUND = "#FFF5E1"
-GENERATE_PASSWORD_COLOR = "#81A263"
-ADD_COLOR = "#365E32"
+FONT = ("Helvetica", 10)
+BACKGROUND = "#FEFBF6"
+SEARCH_COLOR = "#35A29F"
+GENERATE_PASSWORD_COLOR = "#0B666A"
+ACTIVE_BG_COLOR = "#97FEED"
+ADD_COLOR = "#071952"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -29,20 +32,48 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n{email} \nPassword: {password} \nIs it ok to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                 json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)     
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)     
 
-        if is_ok:
-
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-        
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -51,7 +82,7 @@ window.config(padx=50, pady=50, bg=BACKGROUND)
 canvas = Canvas(height=200, width=200, bg=BACKGROUND, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1, columnspan=3)
+canvas.grid(row=0, column=1, columnspan=2)
 
 #Labels
 website_label = Label(text="Website:  ", font=FONT, bg=BACKGROUND)
@@ -64,22 +95,24 @@ password_label = Label(text="Password:  ", font=FONT, bg=BACKGROUND)
 password_label.grid(row=3, column=0, pady=5)
 
 #Entries
-website_entry = Entry(width=45, font=FONT)
-website_entry.grid(row=1, column=1, columnspan=2, pady=5)
+website_entry = Entry(width=27, font=FONT)
+website_entry.grid(row=1, column=1, pady=5)
 website_entry.focus()
 
 email_entry = Entry(width=45, font=FONT)
 email_entry.grid(row=2, column=1, columnspan=2, pady=5)
-email_entry.insert(0, "xyz@email.com")
+email_entry.insert(0, "pragyan@gmail.com")
 
 password_entry = Entry(width=27, font=FONT)
 password_entry.grid(row=3, column=1, pady=5)
 
 #Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password, fg = "white", bg = GENERATE_PASSWORD_COLOR, font=FONT, highlightthickness=0)
+search_button = Button(text="Search", width=14, command=find_password, fg="white", bg=SEARCH_COLOR, activebackground=ACTIVE_BG_COLOR, activeforeground="white", relief="raised", font=FONT, highlightthickness=0)
+search_button.grid(row=1, column=2, pady=5)
+generate_password_button = Button(text="Generate Password", command=generate_password, fg="white", bg=GENERATE_PASSWORD_COLOR, activebackground=ACTIVE_BG_COLOR, activeforeground="white", relief="raised", font=FONT, highlightthickness=0)
 generate_password_button.grid(row=3, column=2, pady=5)
 
-add_button = Button(text="Add", width=39, command=save, fg = "white", bg = ADD_COLOR, font=FONT, highlightthickness=0)
+add_button = Button(text="Add", width=39, command=save, fg = "white", bg=ADD_COLOR, activebackground=ACTIVE_BG_COLOR, font=FONT, relief="raised", highlightthickness=0)
 add_button.grid(row=4, column=1, columnspan=2, pady=5)
 
 window.mainloop()
